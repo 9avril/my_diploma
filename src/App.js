b8220a283ba2693e12a252905e3450c30ce74ee1
fluevel <artemkiasko@gmail.com>
2023-04-18 14:23:57 +0300

    Rabochaya tablica

--- src/App.js
+++ src/App.js
@@ -20,116 +20,190 @@ function App() {
 			headerName: 'Model',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
 		},
 		{
 			field: 'weight',
 			headerName: 'Weight',
 			sortable: true,
 			filter: 'agNumberColumnFilter',
+			autoHeight: true,
+			cellStyle: params => {
+				return {
+					backgroundColor:
+						params.column.colId % 2 === 0 ? '#ffffff' : '#f3f3f3',
+				}
+			},
 		},
 		{
 			field: 'size',
 			headerName: 'Size',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
 		},
 		{
 			field: 'temperature_range',
 			headerName: 'Temperature Range',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
+			cellStyle: params => {
+				return {
+					backgroundColor:
+						params.column.colId % 2 === 0 ? '#ffffff' : '#f3f3f3',
+				}
+			},
 		},
 		{
 			field: 'humidity_range',
 			headerName: 'Humidity Range',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
 		},
 		{
 			field: 'security_info',
 			headerName: 'Security Info',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
+			cellStyle: params => {
+				return {
+					backgroundColor:
+						params.column.colId % 2 === 0 ? '#ffffff' : '#f3f3f3',
+				}
+			},
 		},
 		{
 			field: 'os',
 			headerName: 'OS',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
 		},
 		{
 			field: 'supported_protocols',
 			headerName: 'Supported Protocols',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
+			cellStyle: params => {
+				return {
+					backgroundColor:
+						params.column.colId % 2 === 0 ? '#ffffff' : '#f3f3f3',
+				}
+			},
 		},
 		{
 			field: 'antenna',
 			headerName: 'Antenna',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
 		},
 		{
 			field: 'antenna_type',
 			headerName: 'Antenna Type',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
+			cellStyle: params => {
+				return {
+					backgroundColor:
+						params.column.colId % 2 === 0 ? '#ffffff' : '#f3f3f3',
+				}
+			},
 		},
 		{
 			field: 'ports_info',
 			headerName: 'Ports Info',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
 		},
 		{
 			field: 'power_source',
 			headerName: 'Power Source',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
+			cellStyle: params => {
+				return {
+					backgroundColor:
+						params.column.colId % 2 === 0 ? '#ffffff' : '#f3f3f3',
+				}
+			},
 		},
 		{
 			field: 'wifi_standard',
 			headerName: 'WiFi Standard',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
 		},
 		{
 			field: 'wifi_bandwidth',
 			headerName: 'WiFi Bandwidth',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
+			cellStyle: params => {
+				return {
+					backgroundColor:
+						params.column.colId % 2 === 0 ? '#ffffff' : '#f3f3f3',
+				}
+			},
 		},
 		{
 			field: 'ethernet_speed',
 			headerName: 'Ethernet Speed',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
 		},
 		{
 			field: 'usb_ports',
 			headerName: 'USB Ports',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
+			cellStyle: params => {
+				return {
+					backgroundColor:
+						params.column.colId % 2 === 0 ? '#ffffff' : '#f3f3f3',
+				}
+			},
 		},
 		{
 			field: 'vpn_support',
 			headerName: 'VPN Support',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
 		},
 		{
 			field: 'indoor_coverage_area',
 			headerName: 'Indoor Coverage Area',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
+			cellStyle: params => {
+				return {
+					backgroundColor:
+						params.column.colId % 2 === 0 ? '#ffffff' : '#f3f3f3',
+				}
+			},
 		},
 		{
 			field: 'outdoor_coverage_area',
 			headerName: 'Outdoor Coverage Area',
 			sortable: true,
 			filter: 'agTextColumnFilter',
+			autoHeight: true,
 		},
 	]
+
 	const onGridReady = params => {
 		setGridApi(params.api)
 		setColumnApi(params.columnApi) // Установите columnApi
@@ -137,31 +211,38 @@ function App() {
 	}
 
 	return (
-		<div>
-			<h1>Routers</h1>
-			<div className='ag-theme-alpine' style={{ height: 400, width: '100%' }}>
-				<AgGridReact
-					rowData={routers}
-					columnDefs={routerColumns}
-					defaultColDef={{
-						resizable: true,
-						minWidth: 150,
-						cellStyle: { 'white-space': 'normal', 'line-height': '20px' },
-						sortable: true,
-						filter: true,
-					}}
-					onGridReady={onGridReady}
-					onFirstDataRendered={onGridReady} // Добавьте событие onFirstDataRendered
-					rowSelection='multiple'
-					suppressRowClickSelection
-				/>
+		<div className='flex-container'>
+			<h1 className='font-bold text-3xl mb-5 ml-3'>Routers</h1>
+			<div className='flex'>
+				<div className='ag-theme-alpine w-full ml-3' style={{ flexGrow: 1 }}>
+					<AgGridReact
+						rowData={routers}
+						columnDefs={routerColumns}
+						defaultColDef={{
+							resizable: true,
+							minWidth: 150,
+							cellStyle: { 'white-space': 'normal', 'line-height': '28px' },
+							sortable: true,
+							filter: true,
+							cellClass: params => {
+								return params.colIndex % 2 === 0 ? 'ag-custom-column-bg' : ''
+							},
+						}}
+						onGridReady={onGridReady}
+						onFirstDataRendered={onGridReady} // Добавьте событие onFirstDataRendered
+						rowSelection='multiple'
+						suppressRowClickSelection
+					/>
+				</div>
+				{columnApi && (
+					<div className='w-1/4 ml-4'>
+						<FieldVisibilityController
+							columnApi={columnApi}
+							columnDefs={routerColumns}
+						/>
+					</div>
+				)}
 			</div>
-			{columnApi && (
-				<FieldVisibilityController
-					columnApi={columnApi}
-					columnDefs={routerColumns}
-				/>
-			)}
 		</div>
 	)
 }
--- src/FieldVisibilityController.jsx
+++ src/FieldVisibilityController.jsx
@@ -1,48 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 const FieldVisibilityController = ({ columnApi, columnDefs }) => {
-    const [columnVisibility, setColumnVisibility] = useState({});
+	const [columnVisibility, setColumnVisibility] = useState({})
 
-    useEffect(() => {
-        if (columnApi) {
-            const initialVisibility = {};
-            columnDefs.forEach((col) => {
-                initialVisibility[col.field] = columnApi.getColumn(col.field).isVisible();
-            });
-            setColumnVisibility(initialVisibility);
-        }
-    }, [columnApi, columnDefs]);
+	useEffect(() => {
+		if (columnApi) {
+			const initialVisibility = {}
+			columnDefs.forEach(col => {
+				initialVisibility[col.field] = columnApi
+					.getColumn(col.field)
+					.isVisible()
+			})
+			setColumnVisibility(initialVisibility)
+		}
+	}, [columnApi, columnDefs])
 
-    const handleChange = (field, isChecked) => {
-        if (columnApi) {
-            columnApi.setColumnVisible(field, isChecked);
-            setColumnVisibility({ ...columnVisibility, [field]: isChecked });
-        }
-    };
+	const handleChange = (field, isChecked) => {
+		if (columnApi) {
+			columnApi.setColumnVisible(field, isChecked)
+			setColumnVisibility({ ...columnVisibility, [field]: isChecked })
+		}
+	}
 
-    // Не рендерим компонент, если columnApi еще не установлен
-    if (!columnApi) {
-        return null;
-    }
+	// Не рендерим компонент, если columnApi еще не установлен
+	if (!columnApi) {
+		return null
+	}
 
-    return (
-        <div>
-            <h3>Field Visibility</h3>
-            <ul>
-                {columnDefs.map((col) => (
-                    <li key={col.field}>
-                        <input
-                            type="checkbox"
-                            id={col.field}
-                            checked={columnVisibility[col.field]}
-                            onChange={(e) => handleChange(col.field, e.target.checked)}
-                        />
-                        <label htmlFor={col.field}>{col.headerName}</label>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
-
-export default FieldVisibilityController;
+	return (
+		<div className='border border-gray-200 p-4 rounded bg-gray-200'>
+			<h3 className='text-xl font-semibold mb-4'>Field Visibility</h3>
+			<ul>
+				{columnDefs.map(col => (
+					<li key={col.field} className='flex items-center mb-2'>
+						<input
+							type='checkbox'
+							id={col.field}
+							className='mr-2'
+							checked={columnVisibility[col.field]}
+							onChange={e => handleChange(col.field, e.target.checked)}
+						/>
+						<label htmlFor={col.field} className='text-black'>
+							{col.headerName}
+						</label>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+export default FieldVisibilityController
